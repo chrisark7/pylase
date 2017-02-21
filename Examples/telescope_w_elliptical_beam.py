a6cb77cc27d54a0ccbd44a3c7b4eba87e8b32f08
Chris <chrisark7@gmail.com>
2017-02-21 15:28:03 -0500

    Finished updating example files, removed old packages

--- Examples/telescope_w_elliptical_beam.py
+++ Examples/telescope_w_elliptical_beam.py
@@ -4,8 +4,8 @@ This example defines a simple two-element telescope and two beams, one for x and
 plots the beam size through the system.
 """
 
-import numpy as np
-from pylase.optical_system_old import OpticalSystem
+from numpy import linspace
+from pylase.optical_system import OpticalSystem
 
 __author__ = "Chris Mueller"
 __status__ = "Example"
@@ -13,14 +13,22 @@ __status__ = "Example"
 
 # Create optical system with two lenses
 os = OpticalSystem()
-os.add_thin_lens(f=50e-3, z=100e-3, label='lens 1')
-os.add_thin_lens(f=150e-3, z=300e-3, label='lens 2')
+os.add_element_thin_lens(z=100e-3, label='lens 1', f=50e-3)
+os.add_element_thin_lens(z=300e-3, label='lens 2', f=150e-3)
 
 # Add two beams
-os.add_beam_from_parameters(waist_size=0.1e-3, distance_to_waist=0, wvlnt=1064e-9, z=0, beam_label='x')
-os.add_beam_from_parameters(waist_size=0.2e-3, distance_to_waist=0, wvlnt=1064e-9, z=0, beam_label='y')
+os.add_beam_from_parameters(z=0,
+                            label='x',
+                            beam_size=0.1e-3,
+                            distance_to_waist=0,
+                            wvlnt=1064e-9)
+os.add_beam_from_parameters(z=0,
+                            label='y',
+                            beam_size=0.2e-3,
+                            distance_to_waist=0,
+                            wvlnt=1064e-9)
 
 # Plot
-fig, ax = os.plot_w(zs=np.linspace(0, 500e-3, 500))
+fig, ax = os.plot_w(zs=linspace(0, 500e-3, 500))
 ax.set_title('Elliptical Beam in 3X Telescope')
 fig.show()
